a3d885478800a0bdfc65016ebc47351327cbd85d
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-04-03 22:20:21 -0400

    Cargando DB4O

--- src/db4o/Guardado.java
+++ src/db4o/Guardado.java
@@ -6,6 +6,6 @@ public class Guardado {
     private int id;
     private Usuario usuario;
     private String nombre_album;
-    private Publicacion[] publicacion;
+    private Publicacion publicacion;
     
 }
